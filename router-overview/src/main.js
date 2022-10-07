d8d69467eae3b840d2ffef6f02ce9b7476589f5c
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-10-06 21:27:44 -0300

    concluindo estudos de rotas

--- router-overview/src/main.js
+++ router-overview/src/main.js
@@ -1,11 +1,18 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from "vue-router"
 import App from './App.vue'
+import PageA from "./components/PageA.vue"
+import PageB from "./components/PageB.vue"
+import PageC from "./components/PageC.vue"
+import User from "./components/User.vue"
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
+        { path: "/pageA", component: PageA },
+        { path: "/pageB", component: PageB },
+        { path: "/pageC", component: PageC },
+        { path: "/user/:userId", name: "user", component: User }
     ]
 })
 
