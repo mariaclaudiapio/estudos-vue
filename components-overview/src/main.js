58bbffcb1feb0c8d398b455257a04065895d83ad
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-10-26 23:07:14 -0300

    iniciando projeto Alura Tracker

--- components-overview/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import GlobalComponent from "./components/global-component.vue"
-
-const app = createApp(App)
-app.component("GlobalComponent", GlobalComponent)
-app.mount('#app')
