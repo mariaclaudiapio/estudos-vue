a2ec7f0dda10187aa083f2a4a8d6480bb48a5848
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-10-29 21:27:34 -0300

    Alura Tracker:
    incluindo funcionalidade de Ativar Modo Escuro

--- alura/alura-tracker/src/App.vue
+++ alura/alura-tracker/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
-    <div class="column is-three-quarter">
+    <div class="column is-three-quarter conteudo">
       <Formulario @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">        
         <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
@@ -36,7 +36,8 @@ export default defineComponent({
   },
   data () {
     return {
-      tarefas: [] as ITarefa[]
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
     }
   },
   computed: {
@@ -47,15 +48,31 @@ export default defineComponent({
   methods: {
     salvarTarefa (tarefa: ITarefa) {
       this.tarefas.push(tarefa)
+    },
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo
     }
   }
 });
 </script>
 
 <style>
-
 .lista {
   padding: 1.25rem;
+}
+
+main {
+  --bg-primario: #FFFFFF;
+  --texto-primario: #000000;
+}
+
+main.modo-escuro {
+  --bg-primario: #2B2D42;
+  --texto-primario: #DDDDDD;
+}
+
+.conteudo {
+  background-color: var(--bg-primario)
 }
 
 </style>
--- alura/alura-tracker/src/components/Formulario.vue
+++ alura/alura-tracker/src/components/Formulario.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="box">
+    <div class="box formulario">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
                 <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?"
@@ -41,3 +41,10 @@
     })
 
 </script>
+
+<style>
+.formulario {
+    color: var(--texto-primario);
+    background-color: var(--bg-primario);
+}
+</style>
